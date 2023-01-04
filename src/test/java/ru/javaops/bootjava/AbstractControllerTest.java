de8087d30a7346e70363b5797072835c7f1ef03d
I_M <moskva.88@mail.ru>
2023-01-04 14:08:08 +0300

    6_07_update_cache

--- src/test/java/ru/javaops/bootjava/AbstractControllerTest.java
+++ src/test/java/ru/javaops/bootjava/AbstractControllerTest.java
@@ -3,6 +3,7 @@ package ru.javaops.bootjava;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class AbstractControllerTest {
 
     @Autowired
